7cdb2e75b46c0d4e05e11e3d7ebd0418d31c81eb
Anisha0906 <anishadas880@gmail.com>
2022-07-07 00:10:46 +0530

    Added:two digit sum

---
+++ twodigitsum.sh
@@ -0,0 +1,9 @@
+num1=$((10+ RANDOM%90))
+num2=$((10+ RANDOM%90))
+num3=$((10+ RANDOM%90))
+num4=$((10+ RANDOM%90))
+num5=$((10+ RANDOM%90))
+sum=$((num1+num2+num3+num4+num5))
+echo $sum
+avg=$((sum/5))
+echo $avg
